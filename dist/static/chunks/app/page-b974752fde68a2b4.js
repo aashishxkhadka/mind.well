2f860a1e343cc40f05a807cdae7e9bb6d245a063
aashishxkhadka <aashishkhadka2058@gmail.com>
2025-04-20 17:45:40 +0545

    add dist folder

---
+++ dist/static/chunks/app/page-b974752fde68a2b4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1959,5763,7066,7656,8974],{5405:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,70,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[70,8364,9770,7358],()=>s(5405)),_N_E=e.O()}]);
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import { cloneElement } from "react";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  distDir : "./dist",
    images: {
     remotePatterns: [{
       protocol: "https",
